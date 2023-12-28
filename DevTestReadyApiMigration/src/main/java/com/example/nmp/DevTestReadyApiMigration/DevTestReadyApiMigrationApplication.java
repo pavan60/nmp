0e0e2076da20c4ed467bf894fb16a8d31ae99c69
Pavan60 <pkonathala704@gmail.com>
2023-12-28 12:40:10 +0530

    Initial Commit - Project Setup
    Created an initial Java Spring boot application with default tomcat server running on port 5432.

---
+++ DevTestReadyApiMigration/src/main/java/com/example/nmp/DevTestReadyApiMigration/DevTestReadyApiMigrationApplication.java
@@ -0,0 +1,13 @@
+package com.example.nmp.DevTestReadyApiMigration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevTestReadyApiMigrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevTestReadyApiMigrationApplication.class, args);
+	}
+
+}
---
+++ DevTestReadyApiMigration/src/test/java/com/example/nmp/DevTestReadyApiMigration/DevTestReadyApiMigrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nmp.DevTestReadyApiMigration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevTestReadyApiMigrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
